080199dd3a0ebcc0b826003f9a65e89355120c12
Duke0404 <devansh0404@gmail.com>
2022-07-26 14:22:44 +0200

    Added Data and Components

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,73 @@
-const App = (): JSX.Element => (
-    <div>
-        <h1>Hello World</h1>
-    </div>
-)
+//Icons
+import { BiCalendar, BiTrash } from 'react-icons/bi'
+
+//Data
+import appointmentsData from '../../Data/data.json'
+
+//Components
+import Search from "../Search/Search"
+import AddAppointment from '../AddAppointment/AddAppointment'
+
+//Interfaces
+interface AppointmentInterface {
+	"id": string,
+    "petName": string,
+    "ownerName": string,
+    "aptNotes": string,
+    "aptDate": string
+}
+
+const App = (): JSX.Element => {
+	const appointmentsList: JSX.Element[] = appointmentsData.map(
+		(appointment: AppointmentInterface): JSX.Element => (
+			<li className="px-3 py-3 flex items-start" key={appointment.id}>
+				<button type="button" className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-500hover:bg-yellow-700focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+					<BiTrash />
+				</button>
+
+				<div className="flex-grow">
+					<div className="flex items-center">
+						<span className="flex-none font-medium text-2xl text-blue-500">{appointment.petName}</span>
+						<span className="flex-grow text-right">{appointment.aptDate}</span>
+					</div>
+
+					<div>
+						<b className="font-bold text-blue-500">Owner:</b> {appointment.ownerName}
+					</div>
+
+					<div className="leading-tight">{appointment.aptNotes}</div>
+				</div>
+			</li>
+		)
+	)
+
+	return (
+		<div className='
+			container
+			mx-auto
+			mt-4
+			font-thin
+		'>
+			<h1 className='text-5xl mb-3'>
+				<BiCalendar className='
+					inline-block
+					text-red-400
+					align-top
+				'/> Hello World
+			</h1>
+
+			<AddAppointment />
+
+			<Search />
+
+			<ul className='
+				divide-y
+				divide-gray-200
+			'>
+				{appointmentsList}
+			</ul>
+		</div>
+	)
+}
 
 export default App
---
+++ src/components/DropDown/DropDown.tsx
@@ -0,0 +1,27 @@
+//Icons
+import { BiCheck } from "react-icons/bi"
+
+const DropDown = (): JSX.Element => (
+		<div className="origin-top-right absolute right-0 mt-2 w-56
+		rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+		<div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+			<div
+				className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
+				role="menuitem">Pet Name <BiCheck /></div>
+			<div
+				className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
+				role="menuitem">Owner Name  <BiCheck /></div>
+			<div
+				className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
+				role="menuitem">Date <BiCheck /></div>
+			<div
+				className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer border-gray-1 border-t-2"
+				role="menuitem">Asc <BiCheck /></div>
+			<div
+				className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
+				role="menuitem">Desc <BiCheck /></div>
+		</div>
+	</div>
+)
+
+export default DropDown
